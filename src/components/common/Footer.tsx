9312d5204122b6fce5750eb6dd6fdc0985113dda
Saumitra Bagchi <saumitra.ku.cse@gmail.com>
2026-05-29 13:07:56 +0300

    Footer change from main

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -2,7 +2,7 @@ import bookImage from "../../assets/image/bookImage.png";
 import { BsTwitterX } from "react-icons/bs";
 import { IoLogoFacebook } from "react-icons/io5";
 import { TfiEmail } from "react-icons/tfi";
-// hello from conflit branch
+// hello from Main
 const Footer = () => {
   return (
     
