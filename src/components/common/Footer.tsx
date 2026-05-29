1d2ed64d8b8a8f09354991bd71c25f157d9cf756
Saumitra Bagchi <saumitra.ku.cse@gmail.com>
2026-05-29 14:06:22 +0300

    Footer change from feature

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -2,7 +2,7 @@ import bookImage from "../../assets/image/bookImage.png";
 import { BsTwitterX } from "react-icons/bs";
 import { IoLogoFacebook } from "react-icons/io5";
 import { TfiEmail } from "react-icons/tfi";
-// hello from feature
+// hello from Feature
 const Footer = () => {
   return (
     
